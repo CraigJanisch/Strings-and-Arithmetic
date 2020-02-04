f8a188b354a08e49626aa5830086f4aaf941a25b
Craig Janisch <craigjanisch@icloud.com>
2020-02-04 20:21:45 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,28 @@
+// String Literal
+print("The Boy Cried:\n\n\t\"Wolf!\"")
+
+let string = "The Boy Cried:\n\n\t\"Wolf!\""
+print (string)
+
+//Multi-line string literals
+let multiString = """
+The Boy Cried:
+
+\t "Wolf!"
+"""
+print (multiString)
+
+//Concatenate Strings
+let firstName = "Craig"
+let lastName = "Janisch"
+let fullName = firstName + " " + lastName
+print (fullName)
+
+//String Interpolation
+let name = "Craig"
+let age = 54
+print ("My name is \(name) and I am \(age) years old.")
+
+let days = 2
+let language = "Swift"
+print ("I have been learning \(language) for \(days) days. I am really enjoyiong it!")
